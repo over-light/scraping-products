b576981dc19128cd05888a2b1e2bcb48d4898697
over.light <over.light888@gmail.com>
2023-02-02 20:13:40 +0200

    Create the DataBase

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 from itertools import zip_longest
+
+# Create DB:
+db = sqlite3.connect('products.sqlite')
+open = db.cursor()
+
+open.execute('CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY, title TEXT, price INTEGER)')
